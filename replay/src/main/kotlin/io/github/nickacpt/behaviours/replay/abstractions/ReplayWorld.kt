e621000ce33865936b857b97280abeac6a83ff49
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-01-20 19:26:24 +0000

    Implement items

--- replay/src/main/kotlin/io/github/nickacpt/behaviours/replay/abstractions/ReplayWorld.kt
+++ replay/src/main/kotlin/io/github/nickacpt/behaviours/replay/abstractions/ReplayWorld.kt
@@ -1,7 +1,14 @@
 package io.github.nickacpt.behaviours.replay.abstractions
 
+import io.github.nickacpt.behaviours.replay.model.Replay
 import java.util.*
 
+/**
+ * Represents a world that can be used to play and record a [Replay].
+ */
 interface ReplayWorld {
+    /**
+     * The uuid of the world.
+     */
     val id: UUID
 }
