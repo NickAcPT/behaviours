e621000566afdbd4073bfad05282ca4fee7b8aa0
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-01-23 21:33:17 +0000

    Add documentation

--- replay/src/main/kotlin/io/github/nickacpt/behaviours/replay/abstractions/EntityManager.kt
+++ replay/src/main/kotlin/io/github/nickacpt/behaviours/replay/abstractions/EntityManager.kt
@@ -3,12 +3,36 @@ package io.github.nickacpt.behaviours.replay.abstractions
 import io.github.nickacpt.behaviours.replay.model.RecordedReplayEntity
 import io.github.nickacpt.behaviours.replay.model.standard.location.RecordableLocation
 
+/**
+ * Manages the entities during a replay session.
+ * It provides methods to spawn, update, and remove entities.
+ *
+ * @param Entity The type of entities being managed.
+ */
 interface EntityManager<Entity : ReplayEntity> {
 
+    /**
+     * Spawns an entity at a given location
+     *
+     * @param entity The entity to spawn.
+     * @param location The location to spawn the entity at.
+     * @return The spawned entity
+     */
     fun spawnEntity(entity: RecordedReplayEntity, location: RecordableLocation): Entity
 
+    /**
+     * Update the position of an entity
+     *
+     * @param entity The entity to update
+     * @param location The new location to move the entity to
+     */
     fun updateEntityPosition(entity: Entity, location: RecordableLocation)
 
+    /**
+     * Removes an entity from the world
+     *
+     * @param entity The entity to remove
+     */
     fun removeEntity(entity: Entity)
 
 }
--- replay/src/main/kotlin/io/github/nickacpt/behaviours/replay/playback/Replayer.kt
+++ replay/src/main/kotlin/io/github/nickacpt/behaviours/replay/playback/Replayer.kt
@@ -13,6 +13,7 @@ import io.github.nickacpt.behaviours.replay.playback.session.ReplaySession
  * details of the platform it is being used on.
  *
  * @param Viewer The viewer type of the platform.
+ * @param World The world type of the platform.
  * @param Entity The entity type of the platform.
  * @param Platform The platform in which the replay system exists.
  * @param System The replay system being used.
@@ -30,6 +31,7 @@ interface Replayer<
      * @param replay The replay to prepare the session for.
      * @param replaySession The replay session to prepare.
      * @param replayViewers The list of replay viewers to prepare the session for.
+     * @return The prepared world for the replay session
      */
     fun prepareReplaySession(
         replay: Replay,
@@ -37,6 +39,13 @@ interface Replayer<
         replayViewers: List<Viewer>
     ): World
 
+    /**
+     * Creates an Entity manager for the given replay system and session.
+     *
+     * @param replaySystem The replay system being used.
+     * @param replaySession The replay session to create the Entity manager for.
+     * @return an Entity manager for the given replay system and session.
+     */
     fun <Platform : ReplayPlatform<Viewer, World, Entity>,
             System : ReplaySystem<Viewer, World, Entity, Platform>,
             Session : ReplaySession<Viewer, World, Entity, Platform, System>> createEntityManager(
@@ -44,11 +53,26 @@ interface Replayer<
         replaySession: Session
     ): EntityManager<Entity>
 
+
+    /**
+     * Update the state of the replay session for the given viewer.
+     *
+     * This state usually includes things like the current tick, the current state of the session, etc.
+     *
+     * @param replaySession The replay session to update the state for.
+     * @param viewer The viewer for which to update the state.
+     */
     fun updateReplaySessionStateForViewer(
         replaySession: ReplaySession<Viewer, World, Entity, Platform, System>,
         viewer: Viewer,
     )
 
+    /**
+     * Update the controls of the replay session for the given viewer.
+     *
+     * @param replaySession The replay session to update the controls for.
+     * @param viewer The viewer for which to update the controls.
+     */
     fun updateReplaySessionViewerControls(
         replaySession: ReplaySession<Viewer, World, Entity, Platform, System>,
         viewer: Viewer,
